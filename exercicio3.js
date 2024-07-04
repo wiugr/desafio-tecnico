d5d3e9bfa9aaae7baa10fa3436a963f57292a579
William Rocha <williammrochag@gmail.com>
2024-07-04 19:29:20 -0300

    feat: add estrutura condicional

--- exercicio3.js
+++ exercicio3.js
@@ -1,3 +1,9 @@
 var nota1 = prompt("Insira a primeira nota do aluno: ")
 var nota2 = prompt("Insira a segunda nota do aluno: ")
 var nota3 = prompt("Insira a terceira nota do aluno: ")
+if ((parseFloat(nota1) + parseFloat(nota2) + parseFloat(nota3))/3 >= 6){
+    console.log("Aprovado")
+}
+else{
+    console.log("Reprovado")
+}
